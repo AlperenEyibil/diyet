2906eb70b521fc211e9a49c48f2f37393cc7d0f2
Alperen <eyibil99@gmail.com>
2021-06-08 23:26:55 +0300

    indeks hesaplama tamamlandı(görseller test amaclıdır)

--- lib/screens/weight_control/body_mass_index.dart
+++ lib/screens/weight_control/body_mass_index.dart
@@ -7,14 +7,19 @@ class BodyMassIndex extends StatefulWidget {
 }
 
 class _BodyMassIndexState extends State<BodyMassIndex> {
-
-  TextEditingController _kg=TextEditingController();
-  TextEditingController _boy=TextEditingController();
+  bool isVisible = false;
+  TextEditingController _kg = TextEditingController();
+  TextEditingController _boy = TextEditingController();
   double bmi;
+  String stringBMI = "0";
+  String image = "assets/wally.jpg";
+  String derece = "";
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
@@ -23,25 +28,91 @@ class _BodyMassIndexState extends State<BodyMassIndex> {
       body: Container(
         child: Column(
           children: [
-           calculater(),
+            calculater(),
+            SizedBox(
+              height: 16,
+            ),
+            bmiResult()
           ],
         ),
       ),
     );
   }
 
-  Widget calculater(){
+  Widget bmiResult() {
+    return Visibility(
+      visible: isVisible,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 15.0,
+            ),
+            child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.35,
+                height: MediaQuery.of(context).size.height * 0.6,
+                child: Image.asset(image)),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 70),
+            child: Column(
+              children: [
+                Text(
+                  "İndeks",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Container(
+                  width: 70,
+                  height: 70,
+                  decoration: BoxDecoration(
+                      color: Color(0xFFA9E9F6),
+                      shape: BoxShape.circle,
+                      border: Border.all(width: 2)),
+                  child: Center(
+                    child: Text("${stringBMI}",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 22)),
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                SizedBox(
+                  width: 80,
+                  height: 100,
+                  child: Text(
+                    derece,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget calculater() {
     return Column(
       children: [
         Row(
           children: [
             Flexible(
                 flex: 1,
-                child: SizedBox(width: 70, height: 30,
+                child: SizedBox(
+                  width: 70,
+                  height: 30,
                   child: Text(
                     "Kilo(Kg)",
-                    style: TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
                 )),
             Flexible(
@@ -54,33 +125,37 @@ class _BodyMassIndexState extends State<BodyMassIndex> {
               child: SizedBox(
                 height: 30,
                 width: 65,
-                child: TextField(style: TextStyle(fontSize: 18),
+                child: TextField(
+                    style: TextStyle(fontSize: 18),
                     controller: _kg,
                     maxLines: 1,
-                    decoration: InputDecoration(contentPadding: EdgeInsets.all(6),
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(6),
                       border: OutlineInputBorder(),
                     ),
                     keyboardType: TextInputType.number,
                     inputFormatters: <TextInputFormatter>[
                       WhitelistingTextInputFormatter(RegExp(r'(^\d*\.?\d*)')),
                       LengthLimitingTextInputFormatter(5)
-                    ]
-                ),
+                    ]),
               ),
             )
           ],
           mainAxisAlignment: MainAxisAlignment.center,
         ),
-        SizedBox(height: 16,),
+        SizedBox(
+          height: 16,
+        ),
         Row(
           children: [
             Flexible(
                 flex: 1,
-                child: SizedBox(width: 70,height: 30,
+                child: SizedBox(
+                  width: 70,
+                  height: 30,
                   child: Text(
                     "Boy(m)",
-                    style: TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
                 )),
             Flexible(
@@ -93,28 +168,57 @@ class _BodyMassIndexState extends State<BodyMassIndex> {
               child: SizedBox(
                 height: 30,
                 width: 65,
-                child: TextField(style: TextStyle(fontSize: 18),
+                child: TextField(
+                    style: TextStyle(fontSize: 18),
                     controller: _boy,
                     maxLines: 1,
-                    decoration: InputDecoration(contentPadding: EdgeInsets.all(6),
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(6),
                       border: OutlineInputBorder(),
                     ),
                     keyboardType: TextInputType.number,
                     inputFormatters: <TextInputFormatter>[
                       WhitelistingTextInputFormatter(RegExp(r'(^\d*\.?\d*)')),
                       LengthLimitingTextInputFormatter(4)
-                    ]
-                ),
+                    ]),
               ),
             )
           ],
           mainAxisAlignment: MainAxisAlignment.center,
         ),
-        SizedBox(height: 16,),
-        FlatButton(onPressed: (){
-          bmi=double.parse(_kg.text)/(double.parse(_boy.text)*double.parse(_boy.text));
-          print(bmi);
-        }, child: Text("Hesapla",style: TextStyle(fontSize: 18),),)
+        SizedBox(
+          height: 16,
+        ),
+        FlatButton(
+          onPressed: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+            setState(() {
+              if (_kg.text != "" && _boy.text != "") {
+                isVisible = true;
+                bmi = double.parse(_kg.text) /
+                    (double.parse(_boy.text) * double.parse(_boy.text));
+                stringBMI = bmi.toStringAsFixed(2);
+                if (bmi < 18.5) {
+                  image = "assets/wally.png";
+                  derece = "İdealin altı";
+                } else if (bmi < 25) {
+                  image = "assets/robin.png";
+                  derece = "İdeal";
+                } else if (bmi < 30) {
+                  image = "assets/batman.png";
+                  derece = "İdealin üstü";
+                } else {
+                  image = "assets/unnamed.png";
+                  derece = "İdealin çok üstü";
+                }
+              }
+            });
+          },
+          child: Text(
+            "Hesapla",
+            style: TextStyle(fontSize: 18),
+          ),
+        )
       ],
     );
   }
